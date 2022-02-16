44d9a38388836eacb6e589fe87de8ee4dc6e61ab
etoscano <eleonora.toscano@mail.polimi.it>
2022-02-16 16:50:48 +0100

    setup

---
+++ src/App.js
@@ -0,0 +1,16 @@
+
+import React from "react"
+import Header from "./Components/Header/Header"
+
+function App() {
+
+    return (
+      <>
+        <div className="frame">
+          <Header />
+        </div>
+      </>
+    )
+  }
+
+  export { App }
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom"
+import React from "react"
+
+import "./styles.css"
+
+import "./Global/reset.scss"
+import "./Global/variables.scss"
+import "./Global/main.scss"
+
+import { App } from "./App"
+
+ReactDOM.render(<App />, document.getElementById("root"))
