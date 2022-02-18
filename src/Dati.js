52f091ed87fa8755275159367629dc14317a04e3
etoscano <eleonora.toscano@mail.polimi.it>
2022-02-18 11:37:15 +0100

    projects 1

--- src/Dati.js
+++ src/Dati.js
@@ -23,13 +23,13 @@ const data = {
       },     
       {
         index: 1,
-        title: "222Lorem ipsum dolor sit amet",
+        title: "Lorem ipsum dolor sit amet2",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet nunc fermentum, rhoncus magna vel, ultrices arcu. Nam finibus velit dolor, at semper lorem mattis nec. Phasellus ultricies ante tellus, eget eleifend ex tempus in. Duis pulvinar nulla eu quam malesuada convallis. Ut posuere, odio vel aliquet molestie, nisi eros blandit ligula, vel semper est dolor a nulla. Donec eu blandit metus. Maecenas eu justo accumsan, lobortis tortor non, lacinia ex. Nunc euismod mi nunc, ut vehicula lorem fermentum a.",
         img: "img_2.jpeg"
       },      
       {
         index: 2,
-        title: "333Lorem ipsum dolor sit amet",
+        title: "Lorem ipsum dolor sit amet3",
         description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In laoreet nunc fermentum, rhoncus magna vel, ultrices arcu. Nam finibus velit dolor, at semper lorem mattis nec. Phasellus ultricies ante tellus, eget eleifend ex tempus in. Duis pulvinar nulla eu quam malesuada convallis. Ut posuere, odio vel aliquet molestie, nisi eros blandit ligula, vel semper est dolor a nulla. Donec eu blandit metus. Maecenas eu justo accumsan, lobortis tortor non, lacinia ex. Nunc euismod mi nunc, ut vehicula lorem fermentum a.",
         img: "img_3.jpeg"
       },
